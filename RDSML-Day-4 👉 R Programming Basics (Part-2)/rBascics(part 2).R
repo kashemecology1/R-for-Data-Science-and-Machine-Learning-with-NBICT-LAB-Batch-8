3a217dcdc3f31883cd778633738ff193d928d4e0
kashemecology1 <kashemecologybd@gmail.com>
2025-05-07 22:51:29 +0600

    Day 2 practice

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBascics(part 2).R
@@ -0,0 +1,51 @@
+# Data Types in R
+# 1. Numeric-Integer / Floating point 
+# 2. String/Character- " Kashem"
+# 3. Logical 
+
+a = 11
+b = 7.19
+class(a)
+class(b)
+
+name = "Kashem"
+print(name)
+class(name)
+
+c = "52"
+print(c+9)
+print(a+9)
+class(c)
+
+# Logical Data Type- TRUE, FALSE (T, F)
+class (a > b)
+class (a < b)
+print(a< b)
+x = 19
+y = 22-3
+print (x == y)
+print (x !=y)
+print (y >= x)
+
+
+# Logical operators in R
+# && Logical operator
+# || Logical operator
+
+
+4 == 4 && 5 == 5
+x !=y || x == y
+
+# Simple Mathematical Operators in R
+z = 6 + 9 - 8 * 2 / 3
+print(z)
+
+
+w = 4 + 3 - 3 * 2 / 3
+print (w)
+
+y = 4^2
+print(y)
+
+11 / 5
+11 %% 5
